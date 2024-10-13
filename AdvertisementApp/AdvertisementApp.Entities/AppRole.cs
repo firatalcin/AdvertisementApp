feac65a5b91de2324e27c55054f27827cdd3d7fd
Fırat Alçın <firatalcin04@gmail.com>
2024-10-13 19:13:51 +0300

    AppUser - AppRole - Advertisement

---
+++ AdvertisementApp/AdvertisementApp.Entities/AppRole.cs
@@ -0,0 +1,9 @@
+﻿namespace AdvertisementApp.Entities
+{
+    public class AppRole : BaseEntity
+    {
+        public string Definition { get; set; }
+
+        //public List<AppUserRole> AppUserRoles { get; set; }
+    }
+}
