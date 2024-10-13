49032568f0860298a6864f36de5c74308bd4bd8d
Fırat Alçın <firatalcin04@gmail.com>
2024-10-13 19:07:29 +0300

    Response 2

---
+++ AdvertisementApp/AdvertisementApp.Common/IResponseData.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvertisementApp.Common
+{
+    public interface IResponseData<T> : IResponse
+    {
+        T Data { get; set; }
+        List<CustomValidationError> ValidationErrors { get; set; }
+    }
+}
---
+++ AdvertisementApp/AdvertisementApp.Common/ResponseData.cs
@@ -0,0 +1,26 @@
+﻿using AdvertisementApp.Common.Enums;
+
+namespace AdvertisementApp.Common
+{
+    internal class ResponseData<T> : Response, IResponseData<T> where T : class
+    {
+        public T Data { get; set; }
+
+        public List<CustomValidationError> ValidationErrors { get; set; }
+        public ResponseData(ResponseType responseType, string message) : base(responseType, message)
+        {
+
+        }
+
+        public ResponseData(ResponseType responseType, T data) : base(responseType)
+        {
+            Data = data;
+        }
+
+        public ResponseData(T data, List<CustomValidationError> errors) : base(ResponseType.ValidationError)
+        {
+            ValidationErrors = errors;
+            Data = data;
+        }
+    }
+}
