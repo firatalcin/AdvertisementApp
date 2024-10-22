d9bc11bc2f7f9da90f1e365909d8504cb6503eec
Fırat Alçın <firatalcin04@gmail.com>
2024-10-22 23:39:04 +0300

    Generic Service 1

--- AdvertisementApp/AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
+++ AdvertisementApp/AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -1,5 +1,11 @@
-﻿using AdvertisementApp.DataAccess.Contexts;
+﻿using AdvertisementApp.Business.ValidationRules;
+using AdvertisementApp.DataAccess.Contexts;
 using AdvertisementApp.DataAccess.UnitOfWork;
+using AdvertisementApp.Dtos.AdvertisementAppUserDtos;
+using AdvertisementApp.Dtos.AdvertisementDtos;
+using AdvertisementApp.Dtos.AppUserDtos;
+using AdvertisementApp.Dtos.GenderDtos;
+using AdvertisementApp.Dtos.ProvidedServiceDtos;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -22,16 +28,16 @@ namespace AdvertisementApp.Business.DependencyResolvers.Microsoft
             });
             services.AddScoped<IUOW, UOW>();
 
-            //services.AddTransient<IValidator<ProvidedServiceCreateDto>, ProvidedServiceCreateDtoValidator>();
-            //services.AddTransient<IValidator<ProvidedServiceUpdateDto>, ProvidedServiceUpdateDtoValidator>();
-            //services.AddTransient<IValidator<AdvertisementCreateDto>, AdvertisementCreateDtoValidator>();
-            //services.AddTransient<IValidator<AdvertisementUpdateDto>, AdvertisementUpdateDtoValidator>();
-            //services.AddTransient<IValidator<AppUserCreateDto>, AppUserCreateDtoValidator>();
-            //services.AddTransient<IValidator<AppUserUpdateDto>, AppUserUpdateDtoValidator>();
-            //services.AddTransient<IValidator<AppUserLoginDto>, AppUserLoginDtoValidator>();
-            //services.AddTransient<IValidator<GenderCreateDto>, GenderCreateDtoValidator>();
-            //services.AddTransient<IValidator<GenderUpdateDto>, GenderUpdateDtoValidator>();
-            //services.AddTransient<IValidator<AdvertisementAppUserCreateDto>, AdvertisementAppUserCreateDtoValidator>();
+            services.AddTransient<IValidator<ProvidedServiceCreateDto>, ProvidedServiceCreateDtoValidator>();
+            services.AddTransient<IValidator<ProvidedServiceUpdateDto>, ProvidedServiceUpdateDtoValidator>();
+            services.AddTransient<IValidator<AdvertisementCreateDto>, AdvertisementCreateDtoValidator>();
+            services.AddTransient<IValidator<AdvertisementUpdateDto>, AdvertisementUpdateDtoValidator>();
+            services.AddTransient<IValidator<AppUserCreateDto>, AppUserCreateDtoValidator>();
+            services.AddTransient<IValidator<AppUserUpdateDto>, AppUserUpdateDtoValidator>();
+            services.AddTransient<IValidator<AppUserLoginDto>, AppUserLoginDtoValidator>();
+            services.AddTransient<IValidator<GenderCreateDto>, GenderCreateDtoValidator>();
+            services.AddTransient<IValidator<GenderUpdateDto>, GenderUpdateDtoValidator>();
+            services.AddTransient<IValidator<AdvertisementAppUserCreateDto>, AdvertisementAppUserCreateDtoValidator>();
 
             //services.AddScoped<IProvidedServiceService, ProvidedServiceService>();
             //services.AddScoped<IAdvertisementService, AdvertisementService>();
---
+++ AdvertisementApp/AdvertisementApp.Business/Interfaces/IGenericService.cs
@@ -0,0 +1,28 @@
+﻿using AdvertisementApp.Common;
+using AdvertisementApp.Dtos.Interfaces;
+using AdvertisementApp.Entities;
+using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.Interfaces
+{
+    public interface IGenericService<CreateDto, UpdateDto, ListDto, T>
+        where CreateDto : class, IDto, new()
+        where UpdateDto : class, IUpdateDto, new()
+        where ListDto : class, IDto, new()
+        where T : BaseEntity
+    {
+        Task<IResponseData<CreateDto>> CreateAsync(CreateDto dto);
+        Task<IResponseData<UpdateDto>> UpdateAsync(UpdateDto dto);
+
+        Task<IResponseData<IDto>> GetByIdAsync<IDto>(int id);
+
+        Task<IResponse> RemoveAsync(int id);
+
+        Task<IResponseData<List<ListDto>>> GetAllAsync();
+    }
+}
